7ee4b4d55d789b87957ba9c9985ae6a9cf68de8f
Kennet Vuong <kennet.vuong@knowit.no>
2016-07-19 15:27:12 +0200

    Added bootstrap and jquery and its working

--- src/client.js
+++ src/client.js
@@ -1,3 +1,7 @@
+//import $ from "jquery/jquery"; //Bootstrap jquery
+import "jquery";
+import "bootstrap-sass/assets/javascripts/bootstrap"; //Import javascript bootstrap
+
 import './styles/index'
 
 console.log("Hello client site");
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const projectFolder = process.cwd();
 
@@ -22,14 +23,25 @@ const config = {
             { //Load sass files
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract('style-loader', ["css", "sass"])
-            }
+            },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+            // Bootstrap 3
+            { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' },
         ]
     },
     resolve: {
         extensions: ['', '.js', '.scss'],
     },
     plugins: [
-        new ExtractTextPlugin('[name].css')
+        new ExtractTextPlugin('[name].css'),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery',
+        })
     ],
 };
 
